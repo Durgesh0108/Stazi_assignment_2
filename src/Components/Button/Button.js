57556cab37cd33ed04cb639f2e1b8555946ce164
Durgesh Prajapati <drgaming143@gmail.com>
2023-10-04 02:31:51 +0530

    Final Commit

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = (props) => {
 	return (
 		<button
-			className={`rounded-full p-3 border-indigo-600 text-indigo-600 font-semibold border-2 ${props.className}`}
+			className={`${props.className} rounded-full p-3 border-indigo-600 text-indigo-600 font-semibold border-2 `}
 			onClick={props.onClick}
 		>
 			{props.children}
